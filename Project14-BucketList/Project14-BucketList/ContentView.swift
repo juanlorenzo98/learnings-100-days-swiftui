fe8ecd767390c9ca8672dcad0464f2bbd4d6877b
juanlorenzo98 <juanlorenzo.sumangil@gmail.com>
2025-03-23 01:43:52 +0800

    100 Days of SwiftUI Project14: Bucket List
    Initializing repo for project 14

---
+++ Project14-BucketList/Project14-BucketList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Project14-BucketList
+//
+//  Created by macmini on 3/23/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Project14-BucketList/Project14-BucketList/Project14_BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project14_BucketListApp.swift
+//  Project14-BucketList
+//
+//  Created by macmini on 3/23/25.
+//
+
+import SwiftUI
+
+@main
+struct Project14_BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
