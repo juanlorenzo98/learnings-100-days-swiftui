11d93a2fb5b0887575276a12860cac5f624f5b9b
juanlorenzo98 <juanlorenzo.sumangil@gmail.com>
2025-03-22 21:42:02 +0800

    Building our basic UI

--- Project13-Instafilter/Project13-Instafilter/ContentView.swift
+++ Project13-Instafilter/Project13-Instafilter/ContentView.swift
@@ -6,20 +6,49 @@
 //
 
 import SwiftUI
-import StoreKit
+
 
 struct ContentView: View {
-    @Environment(\.requestReview) var requestReview
+    @State private var  processedImage: Image?
+    @State private var filterIntensity = 0.5
     
     var body: some View {
-        
-        Button("leave review") {
-            requestReview()
+        NavigationStack {
+            VStack {
+                Spacer()
+                
+                if let processedImage {
+                    processedImage
+                        .resizable()
+                        .scaledToFit()
+                } else {
+                    ContentUnavailableView("No Picture", systemImage: "photo.badge.plus", description: Text("Tap to import a photo"))
+                }
+                
+                Spacer()
+                
+                HStack {
+                    Text("Intensity")
+                    Slider(value: $filterIntensity)
+                    
+                }
+                
+                HStack {
+                    Button("Change Filter", action: changeFilter)
+                    
+                    Spacer()
+                    
+                    
+                }
+            }
+            .padding([.horizontal, .bottom])
+            .navigationTitle("InstaFilter")
         }
-
     }
     
-    
+    func changeFilter() {
+        
+    }
 }
 
 #Preview {
