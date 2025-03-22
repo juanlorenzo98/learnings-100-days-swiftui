445f9bbcffe9af810e764de8bd5c61f9f3532461
juanlorenzo98 <juanlorenzo.sumangil@gmail.com>
2025-03-23 06:04:43 +0800

    100 Days of SwiftUI Project16: Hot Prospects
    Introduction

---
+++ Project16-HotProspects/Project16-HotProspects/Project16_HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project16_HotProspectsApp.swift
+//  Project16-HotProspects
+//
+//  Created by macmini on 3/23/25.
+//
+
+import SwiftUI
+
+@main
+struct Project16_HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
