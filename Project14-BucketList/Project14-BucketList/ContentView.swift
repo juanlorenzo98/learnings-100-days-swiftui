5b0836b89cbaee856018627e289a04bc05c38ab0
juanlorenzo98 <juanlorenzo.sumangil@gmail.com>
2025-03-23 04:18:51 +0800

    Integratin MapKit with SwiftUI

--- Project14-BucketList/Project14-BucketList/ContentView.swift
+++ Project14-BucketList/Project14-BucketList/ContentView.swift
@@ -6,46 +6,84 @@
 //
 
 import SwiftUI
+import MapKit
 
-struct LoadingView: View {
-    var body: some View {
-        Text("Loading...")
-    }
-}
-
-struct SuccessView: View {
-    var body: some View {
-        Text("Success!")
-    }
-}
-
-struct FailedView: View {
-    var body: some View {
-        Text("Failed...")
-    }
+struct Location: Identifiable {
+    let id = UUID()
+    var name: String
+    var coordinate: CLLocationCoordinate2D
 }
 
 struct ContentView: View {
-    enum LoadingState {
-        case loading, success , failed
-    }
-
-    @State private var loadingState = LoadingState.loading
+    let locations = [
+        Location(name: "buckingam palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
+        Location(name: "london tower", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
+    ]
+    
+    @State private var position = MapCameraPosition.region(
+        MKCoordinateRegion (
+            center: CLLocationCoordinate2D(latitude: 100, longitude: 100),
+            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
+            )
+        )
+        
     var body: some View {
-//        if loadingState == .loading {
-//            LoadingView()
-//        } else if loadingState == .success {
-//            SuccessView()
-//        } else if loadingState == .failed {
-//            FailedView()
-//        }
-        switch loadingState {
-        case .loading:
-            LoadingView()
-        case .success:
-            SuccessView()
-        case .failed:
-            FailedView()
+        VStack {
+            VStack {
+                MapReader { proxy in
+                    Map()
+                        .onTapGesture {
+                            position in
+                            
+                            if let coordinate = proxy.convert(position, from: .local) {
+                                print(coordinate)
+                            }
+                        }
+                }
+//                Map {
+//                    ForEach(locations) { location in
+////                        Marker(location.name, coordinate: location.coordinate)
+//                        Annotation(location.name, coordinate: location.coordinate) {
+//                            Text(location.name)
+//                                .font(.headline)
+//                                .padding()
+//                                .background(.blue.gradient)
+//                                .foregroundStyle(.white)
+//                                .clipShape(.capsule)
+//                        }
+//                        .annotationTitles(.hidden)
+//                    }
+//                }
+            }
+            
+            
+//            Map(interactionModes: [])
+//                .mapStyle(.hybrid(elevation: .realistic))
+//            Map(position: $position)
+//                .mapStyle(.hybrid(elevation: .realistic))
+//                .onMapCameraChange(frequency: .continuous) { context in
+//                    print(context.region)
+//                }
+            
+            
+            
+//            HStack(spacing: 50) {
+//                Button("Paris") {
+//                    position = MapCameraPosition.region(
+//                        MKCoordinateRegion(
+//                            center: CLLocationCoordinate2D(latitude: 48.8566, longitude: 2.3522),
+//                            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1))
+//                    )
+//                }
+//                
+//                Button("Tokyo") {
+//                    position = MapCameraPosition.region(
+//                        MKCoordinateRegion(
+//                            center: CLLocationCoordinate2D(latitude: 35.6897, longitude: 139.6922),
+//                            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1))
+//                    )
+//                }
+//            }
         }
     }
     
