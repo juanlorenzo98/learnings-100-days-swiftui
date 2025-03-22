aa7d6e7abfa177821df09e0281084dba0cdef494
juanlorenzo98 <juanlorenzo.sumangil@gmail.com>
2025-03-22 20:17:30 +0800

    Showing empty state with ContentUnavailableView

--- Project13-Instafilter/Project13-Instafilter/ContentView.swift
+++ Project13-Instafilter/Project13-Instafilter/ContentView.swift
@@ -5,45 +5,22 @@
 //  Created by macmini on 3/22/25.
 //
 
-import CoreImage
-import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct ContentView: View {
-    @State private var image: Image?
-    var body: some View {
-        VStack {
-            image?
-                .resizable()
-                .scaledToFit()
-        }
-        .onAppear(perform: loadImage)
-    }
     
-    func loadImage() {
-//        let inputImage = UIImage(resource: .example)
-//        let beginImage = CIImage(cgImage: inputImage)
-//        
-//        let context = CIContext()
-//        let currentFilter = CIFilter.twirlDistortion()
-//        
-//        let amount = 1.0
-//        currentFilter.inputImage = beginImage
-//        let inputKeys = currentFilter.inputKeys
-//        
-//        if inputKeys.contains(kCIInputIntensityKey) {
-//            currentFilter.setValue(amount, forKey: kCIInputIntensityKey)}
-//        
-//        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(amount * 200, forKey: kCIInputRadiusKey)}
-//        
-//        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(amount * 10, forKey: kCIInputScaleKey)}
-//        
-//        
-//        guard let outputImage = currentFilter.outputImage else { return }
-//        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
-//        
-//        let uiImage = UIImage(cgImage: cgImage)
-//        image = Image(uiImage: uiImage)
+    var body: some View {
+//        ContentUnavailableView("no snippets", systemImage: "swift", description: "you dont have any saved snippets")
+        ContentUnavailableView {
+            Label("no snippets", systemImage: "swift")
+        } description: {
+            Text("no snippets yet")
+        } actions: {
+            Button("create snippet") {
+                
+            }
+            .buttonStyle(.borderedProminent)
+        }
     }
     
     
