06a16ee743b684cf646ea35915a51bf0a2e4c07b
juanlorenzo98 <juanlorenzo.sumangil@gmail.com>
2025-03-23 03:52:56 +0800

    Switching view states with enums

--- Project14-BucketList/Project14-BucketList/ContentView.swift
+++ Project14-BucketList/Project14-BucketList/ContentView.swift
@@ -7,27 +7,49 @@
 
 import SwiftUI
 
+struct LoadingView: View {
+    var body: some View {
+        Text("Loading...")
+    }
+}
+
+struct SuccessView: View {
+    var body: some View {
+        Text("Success!")
+    }
+}
+
+struct FailedView: View {
+    var body: some View {
+        Text("Failed...")
+    }
+}
 
 struct ContentView: View {
+    enum LoadingState {
+        case loading, success , failed
+    }
 
+    @State private var loadingState = LoadingState.loading
     var body: some View {
-        Button("read and write") {
-            let data = Data("test message".utf8)
-            let url = URL.documentsDirectory.appending(path: "message.txt")
-            
-            do {
-                try data.write(to: url, options: [.atomic, .completeFileProtection])
-                let input = try String(contentsOf: url)
-                print(input)
-            } catch {
-                print(error.localizedDescription)
-            }
+//        if loadingState == .loading {
+//            LoadingView()
+//        } else if loadingState == .success {
+//            SuccessView()
+//        } else if loadingState == .failed {
+//            FailedView()
+//        }
+        switch loadingState {
+        case .loading:
+            LoadingView()
+        case .success:
+            SuccessView()
+        case .failed:
+            FailedView()
         }
     }
     
-    func test() {
-        print(URL.documentsDirectory)
-    }
+
 }
 
 #Preview {
