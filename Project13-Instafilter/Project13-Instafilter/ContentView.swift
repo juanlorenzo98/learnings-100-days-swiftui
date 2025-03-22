ba1fdf8af1b1d15a79a9bf5647dc5f97ad6dea20
juanlorenzo98 <juanlorenzo.sumangil@gmail.com>
2025-03-22 22:27:56 +0800

    Importing an image into SwiftUI using PhotosPicker

--- Project13-Instafilter/Project13-Instafilter/ContentView.swift
+++ Project13-Instafilter/Project13-Instafilter/ContentView.swift
@@ -6,24 +6,33 @@
 //
 
 import SwiftUI
-
+import PhotosUI
 
 struct ContentView: View {
     @State private var  processedImage: Image?
     @State private var filterIntensity = 0.5
+    @State private var selectedItem: PhotosPickerItem?
     
     var body: some View {
         NavigationStack {
             VStack {
                 Spacer()
                 
-                if let processedImage {
-                    processedImage
-                        .resizable()
-                        .scaledToFit()
-                } else {
-                    ContentUnavailableView("No Picture", systemImage: "photo.badge.plus", description: Text("Tap to import a photo"))
+                PhotosPicker(selection: $selectedItem) {
+                    if let processedImage {
+                        processedImage
+                            .resizable()
+                            .scaledToFit()
+                    } else {
+                        ContentUnavailableView("No Picture",
+                                systemImage: "photo.badge.plus",
+                                description: Text("Tap to import a photo"))
+                        
+                    }
                 }
+                .buttonStyle(.plain)
+                .onChange(of: selectedItem, loadImage)
+                
                 
                 Spacer()
                 
@@ -48,6 +57,15 @@ struct ContentView: View {
     
     func changeFilter() {
         
+    }
+    
+    func loadImage() {
+        Task {
+            guard let imageData = try await selectedItem?.loadTransferable(type: Data.self) else { return }
+            
+            guard let inputImage = UIImage(data: imageData) else { return }
+            
+        }
     }
 }
 
