e8eb397167c56a2716975907b9c0dcee2270a167
juanlorenzo98 <juanlorenzo.sumangil@gmail.com>
2025-03-24 03:14:52 +0800

    ScrollView effects using visualEffect() and scrollTargetBehavior()

--- Project18-Geometry/Project18-Geometry/ContentView.swift
+++ Project18-Geometry/Project18-Geometry/ContentView.swift
@@ -14,19 +14,23 @@ struct ContentView: View {
         ScrollView(.horizontal, showsIndicators: false) {
             HStack(spacing: 0) {
                 ForEach(1..<20) { number in
-                    GeometryReader { g in
-                        Text("number \(number)")
-                            .font(.largeTitle)
-                            .padding()
-                            .background(.red)
-                            .rotation3DEffect(.degrees(-g.frame(in: .global).minX / 8), axis: (x: 0, y: 1, z: 0)
-                            )
-                            .frame(width: 200, height: 200)
-                    }
-                    .frame(width: 200, height: 200)
+                    Text("number \(number)")
+                        .font(.largeTitle)
+                        .padding()
+                        .background(.red)
+                        .visualEffect { content, g in
+                            content
+                                .rotation3DEffect(.degrees(-g.frame(in: .global).minX / 8), axis: (x: 0, y: 1, z: 0)
+                                )
+                            
+                        }
+
+                      
                 }
             }
+            .scrollTargetLayout()
         }
+        .scrollTargetBehavior(.viewAligned)
     }
 }
 
