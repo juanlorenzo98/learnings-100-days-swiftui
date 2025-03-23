ea4e188415b74a5fbc3601459f1c334deb8f6bb9
juanlorenzo98 <juanlorenzo.sumangil@gmail.com>
2025-03-23 23:58:03 +0800

    Alignment and alignment guides

--- Project18-Geometry/Project18-Geometry/ContentView.swift
+++ Project18-Geometry/Project18-Geometry/ContentView.swift
@@ -9,9 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("hello world")
-            .padding(20)
-            .background(.red)
+//        Text("hello world")
+//            .frame(width: 300, height: 300, alignment: .topLeading)
+        
+//        HStack(alignment: .lastTextBaseline) {
+//            Text("live")
+//                .font(.caption)
+//            Text("long")
+//            Text("and")
+//                .font(.title)
+//            Text("prospect")
+//                .font(.largeTitle)
+        
+        VStack(alignment: .leading) {
+            ForEach(0..<10) { position in
+                Text("number \(position)")
+                    .alignmentGuide(.leading) { _ in
+                        Double(position) * -10
+                    }
+            }
+        }
+        .background(.red)
+        .frame(width: 400, height: 400)
+        .background(.blue)
     }
 }
 
