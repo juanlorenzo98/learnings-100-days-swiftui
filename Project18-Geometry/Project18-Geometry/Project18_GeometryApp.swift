6184aaf07e8c3c83d0229fe6748e09d48533fc13
juanlorenzo98 <juanlorenzo.sumangil@gmail.com>
2025-03-23 23:32:44 +0800

    100 Days of SwiftUI Project18
    init

---
+++ Project18-Geometry/Project18-Geometry/Project18_GeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project18_GeometryApp.swift
+//  Project18-Geometry
+//
+//  Created by macmini on 3/23/25.
+//
+
+import SwiftUI
+
+@main
+struct Project18_GeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
