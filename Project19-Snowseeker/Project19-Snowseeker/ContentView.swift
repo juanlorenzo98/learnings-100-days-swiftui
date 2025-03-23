a27a55a125adafd071475a66e00a9e31d8797e7e
juanlorenzo98 <juanlorenzo.sumangil@gmail.com>
2025-03-24 03:34:15 +0800

    100 Days of SwiftUI Project19 Snowseeker
    init

---
+++ Project19-Snowseeker/Project19-Snowseeker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Project19-Snowseeker
+//
+//  Created by macmini on 3/24/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Project19-Snowseeker/Project19-Snowseeker/Project19_SnowseekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project19_SnowseekerApp.swift
+//  Project19-Snowseeker
+//
+//  Created by macmini on 3/24/25.
+//
+
+import SwiftUI
+
+@main
+struct Project19_SnowseekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
