f8b85f75d77bca39d4ad718ee57cdc1f6a399307
juanlorenzo98 <juanlorenzo.sumangil@gmail.com>
2025-03-23 23:17:32 +0800

    100 Days of SwiftUI Project 17 Flashzilla
    init

---
+++ Project17-Flashzilla/Project17-Flashzilla/Project17_FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project17_FlashzillaApp.swift
+//  Project17-Flashzilla
+//
+//  Created by macmini on 3/23/25.
+//
+
+import SwiftUI
+
+@main
+struct Project17_FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
